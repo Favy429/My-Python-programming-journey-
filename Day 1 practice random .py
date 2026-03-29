cdf7b20efd45c34d98ea5310066b170fa3d5954d
Favy429 <110993371+Favy429@users.noreply.github.com>
2026-03-29 23:16:15 +0100

    Add files via upload

---
+++ Day 1 practice random .py
@@ -0,0 +1,10 @@
+print(" Hello, Engineer ")
+print("Onyebuchi Favour Chioma ")
+
+print("A chemical process is a method or technique that changes one or more substances into different substances, often involving chemical reactions, like combustion or fermentation. ")
+print("temperature: 50°C ")
+print("pressure: 1atm ")
+print("Reactor status: Normal ")
+
+print("temp_C = 50 ")
+print("temp_F = (temp_C * 9/5 + 32")
